831a9cbf77b24edfa2ebc43ce696c3050eab875d
happle joseph <happlejoseph66@gmail.com>
2026-06-01 22:37:14 +0530

    correct the spelling

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 
 
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const {mongoClient} = require('mongodb');
+const http = require('http');  // create server
+const url = require('url');    // get pathname
+const fs = require('fs');      // read files
+const {mongoClient, MongoClient} = require('mongodb'); // connect MongoDB
 
 const port = 3001;
+
+const client = new MongoClient();
+
+const app = http.createServer((req,res)=> {
+
+    const {pathname} = url.parse(req.url);
+    if(pathname === '/') {
+        res.writeHead(200, {'content-type':'text/html'})
+        res.end(fs.)
+    }
+
+})
